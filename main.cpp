03a97c6917a1f54bc17a73ca372bb146a199b58f
OrlowskiWojtek <owoj@vp.pl>
2023-05-12 11:57:43 +0200

    Com1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
+#include <string>
+#include <tuple>
 
 using namespace std;
 
@@ -13,43 +15,28 @@ double g(double x)
     return sin(x);
 }
 
-double approx1D(int N, double a, double b)
+tuple <double,double> MC1D(int N, double a, double b)
 {
     double S=0;
     for(int i = 0;i<N;i++)
     {
         S += g(a+(b-a)*rand()/(RAND_MAX+1.));
     }
-    return (double) S*((b-a)/(double)N);
+    return {(double) S*((b-a)/(double)N),0};
 }
 
-double approx2D(int N)
+void approx1D(int N, double a, double b, double intVal)
 {
-    double S=0;
-    for(int i = 0;i<N;i++)
-    {
-        S += g(rand()/((double)RAND_MAX) + rand()/((double)RAND_MAX));
-    }
-    return (double) S/N;
-}
-
-int main()
-{
-    srand(time(NULL));
-
     fstream file;
-
 
     system("touch data.dat");
     file.open("data.dat");
 
     cout << "Aproksymacja wielu całek dla różnej ilości próbek:" << endl;  
 
-    int N = 1000000;
-
-    for(int i = 0; i <N; i+= 1000)
+    for(int i = 0; i < N; i+= 1000)
     {
-        file << i << " " <<approx1D(i,0,M_PI) << " " << 2 << "\n"; 
+        file << i << " " << get<0>(MC1D(i,a,b)) << " " << intVal << "\n"; 
         cout << "In progress: " << (double)i/N*100 << "%" << "\n";
     }
 
@@ -57,23 +44,36 @@ int main()
 
     file.close();
 
+}
+
+void app1sam(int N, double a, double b, double intVal)
+{
+    fstream file;
+
     cout << "Aproksymacja jednej całki -> pattern" << endl;
  
     system("touch data2.dat");
     file.open("data2.dat");
     
-    double a = 0; double b = M_PI;
-    N = 100000; double S=0;
+    double S=0;
     
     for(int i = 1;i<N;i++)
     {
         S += g(a+(b-a)*rand()/(RAND_MAX+1.));
-        file << i << " " << S*((b-a)/(double)i) << " " << 2 << endl;
+        file << i << " " << S*((b-a)/(double)i) << " " << intVal << endl;
         cout << "In progress: " << (double)i/N*100 << "%" << "\n";
     }
 
     file.close();
+}
 
+int main()
+{
+    srand(time(NULL));
+
+    approx1D(1e6,0,M_PI,2);
+    app1sam(1e4,0,M_PI,2);
+    
     system("gnuplot < script.gnu");
     system("rm data.dat && rm data2.dat");
 
